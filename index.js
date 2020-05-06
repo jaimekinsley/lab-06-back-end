d2ac78f0436df49a8f50b3780df2ec61bbaecdbc
smooto <melissasmoot@live.com>
2020-05-05 17:30:05 -0700

    init hardcoded data for verification

---
+++ index.js
@@ -0,0 +1,29 @@
+// set up dependencies
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+
+// import data
+const data = require('./data/weather.json');
+// hardcoded until we get the API stuff working
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get('/location', (req, res) => {
+    console.log(`you've reached /location!`)
+    res.json([
+        {
+        "formatted_query": "Seattle, WA, USA",
+        "latitude": "47.606210",
+        "longitude": "-122.332071"
+        }
+    ]);
+});
+
+app.listen(PORT, () => { console.log(`hello from port ${PORT}`) })
