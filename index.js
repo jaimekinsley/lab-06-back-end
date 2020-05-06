162c63f573a67db1d24cea6a3df6fb1218d2f678
smooto <melissasmoot@live.com>
2020-05-05 18:16:27 -0700

    add location endpoint with data for verification

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ const data = require('./data/weather.json');
 // hardcoded until we get the API stuff working
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 
 app.get('/location', (req, res) => {
-    console.log(`you've reached /location!`)
-    res.json([
-        {
-        "formatted_query": "Seattle, WA, USA",
-        "latitude": "47.606210",
-        "longitude": "-122.332071"
-        }
-    ]);
+  console.log(`you've reached /location!`);
+  res.json(
+    {
+      formatted_query: 'Seattle, WA, USA',
+      latitude: '47.606210',
+      longitude: '-122.332071',
+    },
+  );
 });
 
-app.listen(PORT, () => { console.log(`hello from port ${PORT}`) })
+app.listen(PORT, () => { console.log(`hello from port ${PORT}`); })
