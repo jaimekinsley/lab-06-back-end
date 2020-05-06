68f721cf1732bacf82235d73074ff1a3ad913772
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-05 18:51:12 -0700

    adds weather and error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3002;
 app.use(cors());
 
 app.get('/location', (req, res) => {
-  console.log(`you've reached /location!`);
+  console.log('you\'ve reached /location!');
   res.json(
     {
       formatted_query: 'Seattle, WA, USA',
@@ -26,4 +26,38 @@ app.get('/location', (req, res) => {
   );
 });
 
-app.listen(PORT, () => { console.log(`hello from port ${PORT}`); })
+app.get('/weather', (req, res) => {
+  console.log('you\'ve reached /weather!');
+  // try {
+    res.json(
+      [{
+        forecast: 'Partly cloudy until afternoon.',
+        time: 'Mon Jan 01 2001',
+      },
+      {
+        forecast: 'Mostly cloudy in the morning.',
+        time: 'Tue Jan 02 2001',
+      }],
+    );}
+//   } catch (e) {
+//     console.error(e);
+
+//     res.json({
+//       status: 500,
+//       responseText: e,
+
+//     });
+//   }
+// });
+
+// wildstar matches ANYTHING
+app.get('*', (req, res) => {
+  // req will have query parameters and some user data
+
+  // res.json is like the RETURN statement
+  res.status(404).json({
+    error: 'no such route!',
+  });
+});
+
+app.listen(PORT, () => { console.log(`hello from port ${PORT}`); });
